4c1be7561c8a4dda43056de77ec3e8a42afa3820
thennarasank <thennarasan.asus@gmail.com>
2026-02-05 00:17:41 +0530

    Create bit_to_byte_casting.sv

---
+++ Data Types/Casting/bit_to_byte_casting.sv
@@ -0,0 +1,15 @@
+module bit_to_byte_casting();
+  byte a;
+  bit [3:0]b;
+  
+  initial begin
+    b = 4'b1010;
+    $display("\nBefore Casting\n");
+    $display("\nBit Value	: %0d (%b)",b, b);
+    $display("\nByte Value	: %0d (%b)",a, a);
+    a = byte'(a);
+    $display("\nAfter Casting\n");
+    $display("\nBit Value	: %0d (%b)",b, b);
+    $display("\nByte Value	: %0d (%b)",a, a);
+  end
+endmodule
