72dd15599c2a6090cea304fadcfdddc64485a859
thennarasank <thennarasan.asus@gmail.com>
2026-02-10 12:50:12 +0530

    Create packed_array.sv

---
+++ 02.Array/Packed array/packed_array.sv
@@ -0,0 +1,10 @@
+module packed_array;
+   logic [7:0] data;
+  initial begin
+    data = 8'h21;
+
+    $display("Full data = %b", data);
+    $display("Bit 0 = %b", data[0]);
+    $display("Bit 7 = %b", data[7]);
+  end
+endmodule
