4f22f3c1085e8f4327b90c4ccf0a8daa0908562e
thennarasank <thennarasan.asus@gmail.com>
2026-02-11 16:21:05 +0530

    Update string.sv

--- 01.Data Types/String/string.sv
+++ 01.Data Types/String/string.sv
@@ -1,22 +1,16 @@
 module string_();
   string str1;
   string str2;
-  
   initial begin
     str1="HELLO WORLD";
     str2="HeLlO WOrLd";
-    
     $display("\nString1: %s",str1);
     $display("String2: %s",str2);
-    
     if(str1.compare(str2)!=1)
       str2=str2.toupper();
-    
     $display("\nString1: %s",str1);
     $display("String2: %s\n",str2);
-    
     $display("Length of str1: %0d and str2: %0d\n",str1.len(),str2.len());
-    
     $display("Greetings: %s\n",str1.substr(0,4));
   end
 endmodule
