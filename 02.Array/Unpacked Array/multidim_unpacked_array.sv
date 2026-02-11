bcf1ff6f53702957730e0615846a8ada419c03b5
thennarasank <thennarasan.asus@gmail.com>
2026-02-11 16:24:03 +0530

    Update multidim_unpacked_array.sv

--- 02.Array/Unpacked Array/multidim_unpacked_array.sv
+++ 02.Array/Unpacked Array/multidim_unpacked_array.sv
@@ -1,10 +1,8 @@
 module multidim_unpacked_array;
  int king[3][4];  
-
   initial begin
     foreach (king[i, j])
       king[i][j] = i*10 + j;
-    
     foreach (king[i, j])
       $display("king[%0d][%0d] = %0d", i, j,king[i][j]);
   end
