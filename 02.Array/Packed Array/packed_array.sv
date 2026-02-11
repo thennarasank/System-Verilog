25536cb4aa4ca22c6c9c4f3a8676f2e0bac1fe61
thennarasank <thennarasan.asus@gmail.com>
2026-02-11 16:22:03 +0530

    Update packed_array.sv

--- 02.Array/Packed Array/packed_array.sv
+++ 02.Array/Packed Array/packed_array.sv
@@ -2,7 +2,6 @@ module packed_array;
    logic [7:0] data;
   initial begin
     data = 8'h21;
-
     $display("Full data = %b", data);
     $display("Bit 0 = %b", data[0]);
     $display("Bit 7 = %b", data[7]);
