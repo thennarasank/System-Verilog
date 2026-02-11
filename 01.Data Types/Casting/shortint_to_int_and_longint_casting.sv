2b3920341542211f03d6717e9d93156a4e33261d
thennarasank <thennarasan.asus@gmail.com>
2026-02-11 16:19:08 +0530

    Update shortint_to_int_and_longint_casting.sv

--- 01.Data Types/Casting/shortint_to_int_and_longint_casting.sv
+++ 01.Data Types/Casting/shortint_to_int_and_longint_casting.sv
@@ -2,7 +2,6 @@ module shortint_to_int_and_longint_casting();
   int i;
   shortint si;
   longint li;
-  
   initial begin
     si = 'b11111;
     $display("\nShort Int value before casting			: %0d (%b)\n",si,si);
