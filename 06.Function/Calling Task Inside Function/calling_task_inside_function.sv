682d27ababfd6fdc70ecf61972fe860bbe22e3bc
Thennarasan K <thennarasan.asus@gmail.com>
2026-02-18 23:44:55 +0530

    Create calling_task_inside_function.sv

---
+++ 06.Function/Calling Task Inside Function/calling_task_inside_function.sv
@@ -0,0 +1,20 @@
+`timescale 10ns/1ps;
+
+module calling_task_inside_function;
+initial begin
+  $display("\t@ %0t ns, In the initial block",$time);  
+  $display("\tcalling function");  
+  #1 void'(function_call);  
+end  
+function function_call;  
+  fork  
+    $display( "\t@ %0t ns I'm in function",$time);  
+    $display("\t@ %0t ns, calling task from func",$time);  
+    task_call;  
+  join_none  
+endfunction  
+task task_call;  
+  #1 $display( "\t@ %0t ns , I'm in task",$time);  
+  #1 $display("\t@ %0t ns,leaving from task",$time);  
+endtask  
+endmodule
